d5dd684ee84f174bc1555f75e48caeeb0de70ca6
BeingPool <62443996+BeingPool@users.noreply.github.com>
2020-06-27 20:43:56 -0400

    Starting the foundation for card animations

---
+++ projects/vanilla/resources/js/script.js
@@ -0,0 +1,4 @@
+const cardOne = document.getElementById('threeCardOne');
+const cardTwo = document.getElementById('threeCardTwo');
+const cardThree = document.getElementById('threeCardThree');
+const cardArray = [cardOne, cardTwo, cardThree];
